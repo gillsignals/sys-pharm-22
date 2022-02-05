17d866a73ab45824bd86583fb98541562a3f09dc
fmacg <64333654+fmacg@users.noreply.github.com>
2022-02-05 11:05:53 -0500

    refined comments

--- module1_pk/Feb3_1C_model_v1/m1C_eqns.m
+++ module1_pk/Feb3_1C_model_v1/m1C_eqns.m
@@ -27,11 +27,11 @@ V=p(2); % there are easier ways to do this, we've written it this way
 k=p(3); % to emphasize the concept of passing parameters;
 % later we'll demonstrate other ways to do this
 
-dydt = zeros(1,1);    
-% make it a column vector, i.e. (n,1) where n = number of equations
-
 %% EQUATIONS
+% initialize dydt to be the right size
+% make it a column vector, i.e. (n,1) where n = number of equations
+dydt = zeros(1,1);    
 
-% dydt(1) = q - k*y(1); % this equation for q in units of nM/hr
- dydt(1) = q/V - k*y(1) ; % this equation for q in units of nmol/hr
+% dydt(1) = q - k*y(1); % use this equation for q in units of nM/hr
+dydt(1) = q/V - k*y(1) ; % use this equation for q in units of nmol/hr
  
